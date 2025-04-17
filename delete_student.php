ae313252c027c92efe2eb46cc5164e501203abad
Renee Low <reneelow0205@gmail.com>
2025-04-17 10:34:25 +0100

    delete button now does its job correctly

---
+++ delete_student.php
@@ -0,0 +1,19 @@
+<?php
+require 'db.php';
+
+// checks if student_id is provided so script runs only id a valid id is passed
+if (isset($_GET['student_id'])) {
+    $student_id = $conn->real_escape_string($_GET['student_id']);
+
+    // Delete the student with the specified id from the database
+    $sql = "DELETE FROM students WHERE student_id = '$student_id'";
+    if ($conn->query($sql) === true) {
+        // Redirects back to view_content.php 
+        header("Location: view_content.php");
+        exit;
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+
+?>
